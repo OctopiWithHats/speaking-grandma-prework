9ea03cc7d17236528d11ce35b16b2c4cad5ea1a7
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-09 17:48:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,3 +14,10 @@
 
 def speak_to_grandma(words)
   if words = "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  else if words == words.upcase
+    "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end
